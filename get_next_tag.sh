340d7913f25630935b1c9fb8401545b1fe0023d9
Antoine Lempereur <antoine.lempereur@epitech.eu>
2020-04-02 18:39:45 +0200

    update ci conf

--- get_next_tag.sh
+++ get_next_tag.sh
@@ -2,12 +2,12 @@
 
 usage() {
     echo "USAGE:"
-    echo "./get_next_tag.sh BRANCH_NAME CURRENT_TAG"
-    echo "BRANCH_NAME should have the format <RELEASE|FEATURE|FIX>_<any-branch-name>"
+    echo "./get_next_tag.sh COMMIT_MESSAGE CURRENT_TAG"
+    echo "COMMIT_MESSAGE should have the format Merge pull request <PR #> from Yuruh/<PR Name>"
     echo "CURRENT_TAG should have the format <major>.<minor>.<fix>"
 }
 
-branch=$1
+commit_msg=$1
 tag=$2
 
 if [ $# -lt 2 ]
@@ -17,8 +17,6 @@ then
 fi
 
 
-#find position of "_" in the branch name
-underscore_position=$(gawk -v a=$branch -v b="_" "BEGIN{print index(a,b)}")
 
 # Coud be refactored to find n value
 major=$(gawk 'match($0, /[0-9]+/) {
@@ -31,36 +29,24 @@ minor=$(gawk 'match($0, /[0-9]+/) {
   print substr(remaining, RSTART, RLENGTH)
 };' <<< $tag)
 
-fix=$(gawk 'match($0, /[0-9]+/) {
-  remaining=substr($0, RLENGTH + 2)
-}; match(remaining, /[0-9]+/) {
-  remaining=substr(remaining, RLENGTH + 2)
-}; match(remaining, /[0-9]+/) {
-  print substr(remaining, RSTART, RLENGTH)
-};' <<< $tag)
 
-branch_type=$(gawk -v a=$branch -v b=$underscore_position "BEGIN{print substr(a,0,b - 1)}")
+name_position=$(gawk -v a="$commit_msg" -v b="Yuruh/" "BEGIN{print index(a,b)}")
+let name_position+=6
 
-#echo "Version: major=$major minor=$minor fix=$fix"
-#echo "Branch type=$branch_type"
+branch_type=$(gawk -v a="$commit_msg" -v b=$name_position "BEGIN{print substr(a,b,5)}")
 
-if [ $underscore_position -gt 0 ]
+if [ $name_position -gt 0 ]
 then
-  if [ $branch_type = "RELEASE" ]
+  if [ $branch_type = "MAJOR" ]
   then
     let "major+=1"
     let "minor=0"
-    let "fix=0"
-  elif [ $branch_type = "FEATURE" ]
+  elif [ $branch_type = "MINOR" ]
   then
     let "minor+=1"
-    let "fix=0"
-  elif [ $branch_type = "FIX" ]
-  then
-    let "fix+=1"
   fi
 fi
 
-result_version="$major.$minor.$fix"
+result_version="$major.$minor"
 
 echo "$result_version"
